101c444962a3a645d7b688feefcb70f7d782e4eb
Miguel Rivero <miguelriveroloop@gmail.com>
2020-09-18 17:07:39 +0200

    iconList component ready

--- src/components/IconListItem.svelte
+++ src/components/IconListItem.svelte
@@ -9,28 +9,40 @@
     letter-spacing: 1px;
     line-height: 1.5rem;
   }
-  .machine {
+  li {
+    position: relative;
+    padding-left: 4rem;
+    margin-bottom: 0.75rem;
     &:after {
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      content: "#{$assetsUrl}backgrounds/earth_L.jpg";
       position: absolute;
       top: 0;
       left: 0;
-      height: 2.5rem;
-      width: 2.5rem;
-      border: 2px solid #f9f9f9;
-      border-radius: 50%;
-      font-weight: 700;
-      font-size: 1.71429em;
-      line-height: 1.33333;
+      height: 3rem;
+      width: 3rem;
     }
   }
-  .subscriptionHero__pointTitle p {
-    color: #8d1f3c;
-    font-family: $lucasBold;
-    font-size: 1rem;
+  .machine {
+    &:after {
+      content: url("#{$imagesGitStorage}/icons/machine_icon.svg");
+    }
+  }
+  .myaccount {
+    &:after {
+      content: url("#{$imagesGitStorage}/icons/myaccount_icon.svg");
+    }
+  }
+  .cups {
+    &:after {
+      content: url("#{$imagesGitStorage}/icons/cups_icon.svg");
+    }
+  }
+  .subscriptionHero__pointTitle {
+    padding-top: 0.75rem;
+    p {
+      color: #8d1f3c;
+      font-family: $lucasBold;
+      font-size: 1rem;
+    }
   }
   .subscriptionHero__pointText p {
     color: #000000;
